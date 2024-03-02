ba9a73f1b8fb01607e66878b2524cfcc7ab3b46a
DAbarboza <bardanii7@gmail.com>
2024-03-02 17:52:01 -0300

    Act 2/3/24

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Cards from "./components/Cards/Cards.jsx";
 import Nav from "./components/Nav/Nav.jsx";
+import About from "./components/About/About.jsx";
 // import SearchBar from "./components/SearchBar/SearchBar.jsx";
 // import characters from "./data.js";
 import { useState } from "react";
 import axios from "axios";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [characters, setCharacters] = useState([]);
@@ -32,7 +34,14 @@ function App() {
     <div className="App">
       {/* <SearchBar onSearch={(characterID) => window.alert(characterID)} /> */}
       <Nav onSearch={onSearch} />
-      <Cards characters={characters} onClose={onClose} />
+      <Routes>
+        <Route
+          path="/home"
+          element={<Cards characters={characters} onClose={onClose} />}
+        />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      {/* <Cards characters={characters} onClose={onClose} /> */}
     </div>
   );
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import s from "./nav.module.css";
+import { useNavigate, Link } from "react-router-dom";
 
 const Nav = ({ onSearch }) => {
+  const navigate = useNavigate();
   return (
     <div className={s.container}>
+      <button onClick={() => navigate("/home")}>HOME</button>
+      <button onClick={() => navigate("/about")}>ABOUTE</button>
       <SearchBar className={s.searchBar} onSearch={onSearch} />
     </div>
   );
