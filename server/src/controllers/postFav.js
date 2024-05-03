b9cba243f8fa4f2f2f8a5d432ee41d2e4ef63265
DAbarboza <bardanii7@gmail.com>
2024-05-02 22:33:40 -0300

    Act 2/5/24

---
+++ server/src/controllers/postFav.js
@@ -0,0 +1,30 @@
+const { Favorite } = require("../../models/Favorite");
+
+exports.postFav = async (req, res) => {
+  const { id, name, status, species, gender, origin, image } = req.body;
+
+  try {
+    if (!id || !name || !status || !species || !gender || !origin || !image)
+      return res.status(400).json({ message: "Faltan datos" });
+
+    const [charFav, created] = await Favorite.findOrCreate({
+      where: { id },
+      defaults: {
+        name,
+        status,
+        species,
+        gender,
+        origin,
+        image,
+      },
+    });
+
+    if (!created) {
+      return res.status(409).json({ message: "Ya existe" });
+    }
+    const allFavs = await Favorite.findAll();
+    return res.status(200).json(allFavs);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
