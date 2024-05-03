a5812b9cb668d822858bf0f7da8de5b47d5fa15c
DAbarboza <bardanii7@gmail.com>
2024-05-02 22:11:00 -0300

    Act 2/5/24

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,13 @@
+require("./DB_connection");
 const server = require("./app");
-
 const PORT = 3001;
+const { conn } = require("./DB_connection");
 
-server.listen(PORT, () => {
-  console.log(`Server raised in port: ${PORT}`);
-});
+try {
+  server.listen(PORT, async () => {
+    console.log(`Server raised in port: ${PORT}`);
+    await conn.sync({});
+  });
+} catch (error) {
+  console.log(error);
+}
